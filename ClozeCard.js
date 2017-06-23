5ead71f196e003376bd9fba12ec917edf3adf340
Adam Batimana <adambatimana@Adams-iMac.fios-router.home>
2017-06-23 14:27:41 -0400

    throw error and scope safe

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,10 +1,15 @@
-let ClozeCard = function(text,cloze){
+let ClozeCard = function(text,cloze,err){
+  if (err) throw err;
+  if(this instanceof ClozeCard){
   this.partial = ("..." + text.split(cloze)),
   this.fullText = text,
   this.cloze = cloze
-  // if (!cloze) {
-  //   throw
-  // }
+
+}
+    else {
+      return new CloseCard (text,cloze)
+    }
+
 }
 
 module.exports = ClozeCard;
