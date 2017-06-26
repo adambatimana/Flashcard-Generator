21dd9dc8b3c91ad3e389a6bd16ac7e9c400c66c4
Adam Batimana <adambatimana@Adams-iMac.fios-router.home>
2017-06-26 18:28:54 -0400

    More Flash cards

--- flashCards.js
+++ flashCards.js
@@ -5,15 +5,46 @@ const clozeCard = require('./ClozeCard.js')
 // ==================== Basic Card =========================
 // =========================================================
 
+let createBatman = new basicCard (
+  "Who created Batman comics?",
+  "Bob Kane"
+);
+
+console.log("==========================================")
+console.log(createBatman.front)
+console.log(createBatman.back)
+
 let jasonTodd = new basicCard (
   "Who is behind the Red Hood Mask?",
   "Jason Todd"
 );
 
 //WHO IS RED HOOD?
+console.log("==========================================")
 console.log(jasonTodd.front)
 console.log(jasonTodd.back)
 
+let DamianWayne = new basicCard (
+  "Which Robin is Batman's son?",
+  "Damian Wayne"
+);
+
+console.log("==========================================")
+console.log(DamianWayne.front)
+console.log(DamianWayne.back)
+
+let theFlash = new basicCard (
+  "Who is the Fastest man alive?",
+  "The Flash"
+);
+
+console.log("==========================================")
+console.log(theFlash.front)
+console.log(theFlash.back)
+
+
+
+console.log("==========================================")
 
 // =========================================================
 // ==================== cloze Card =========================
@@ -23,9 +54,56 @@ let jasonToddCloze = new clozeCard (
   "Jason Todd"
 );
 
+console.log("==========================================")
+
 //"Jason Todd"
 console.log(jasonToddCloze.cloze)
 //"....is behind the Red Hood Mask"
 console.log(jasonToddCloze.partial)
+
+console.log("==========================================")
+
 //"Jason Todd is behind the Red Hood mask"
 console.log(jasonToddCloze.fullText)
+
+let createBatmanCloze = new clozeCard (
+  "Bob Kane created Batman comics",
+  "Bob Kane"
+);
+
+console.log("==========================================")
+
+console.log(createBatmanCloze.cloze)
+console.log(createBatmanCloze.partial)
+
+console.log("==========================================")
+
+console.log(createBatmanCloze.fullText)
+
+let damianWayneCloze = new clozeCard (
+  "Damian Wayne is the Robin who is Batman's Son",
+  "Damian Wayne"
+);
+
+console.log("==========================================")
+
+console.log(damianWayneCloze.cloze)
+console.log(damianWayneCloze.partial)
+
+console.log("==========================================")
+
+console.log(damianWayneCloze.fullText)
+
+let theFlashCloze = new clozeCard (
+  "The Flash is the fastest man alive",
+  "The Flash"
+);
+
+console.log("==========================================")
+
+console.log(theFlashCloze.cloze)
+console.log(theFlashCloze.partial)
+
+console.log("==========================================")
+
+console.log(theFlashCloze.fullText)
